5fe6e12eacf4e6dc54ef9b506b0f70ed5906904a
Will Sommers <will.sommers@gmail.com>
2014-04-23 09:06:59 -0400

    Add the ability to select specific lines to display.

--- src/draw_me/components/history.cljs
+++ src/draw_me/components/history.cljs
@@ -1,12 +1,13 @@
 (ns draw-me.components.history
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [draw-me.app-state :as app-state]))
 
 (defn line-history [data owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil (dom/div nil (:index data))))))
+      (dom/div nil (dom/div nil (str (:index data) " " "Line"))))))
 
 (defn history-viewer [data owner]
   (reify
@@ -14,4 +15,7 @@
     (render [_]
       (dom/div #js {:className "history"}
                (dom/div nil "History")
-               (apply dom/div nil (om/build-all line-history data))))))
+               (apply dom/div #js {:onClick (fn [e] (do
+                                                   (println (:selected-lines @app-state/app-state))
+                                                   (om/update! data :selected-lines [])))}
+                      (om/build-all line-history (:complete-lines data)))))))
