f2898084981a467f56c2835ef55051458f4d2f91
Will Sommers <will.sommers@gmail.com>
2014-07-09 16:56:40 -0400

    Bump to CLJ/CLJS/Om versions, set up weasel repl.

--- project.clj
+++ project.clj
@@ -2,19 +2,24 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2234"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0"]
-                 [ankha "0.1.1"]]
+                 [om "0.6.4"]
+                 [ankha "0.1.3"]
+                 [prismatic/om-tools "0.2.2"]
+                 [com.cemerick/piggieback "0.1.3"]
+                 [weasel "0.3.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
-  :source-paths ["src"]
+  :profiles {:dev {:source-paths ["src" "dev"]}}
 
-  :cljsbuild { 
-    :builds [{:id "draw-me"
-              :source-paths ["src"]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src" "dev"]
               :compiler {
                 :output-to "draw_me.js"
                 :output-dir "out"
